ea7ef0fbde55876ab2fe8cc14d4a55d69aacc5ae
Mohammed Ariq Rahman <ariqrahman00@gmail.com>
2022-06-05 21:13:49 +0100

    basic app

--- api/src/app.py
+++ api/src/app.py
@@ -1,3 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template
+import uuid
 
 app = Flask(__name__)
+app.secret_key = "such_a_secret"
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    session["uuid"] = str(uuid.uuid4())
+    print(session)
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
