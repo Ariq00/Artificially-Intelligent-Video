f0d3d25a6b392c256da9390d3ecbef6e12638778
Ariq00 <68235142+Ariq00@users.noreply.github.com>
2022-06-07 04:40:27 +0100

    Implemented chatbot with clickable timestamps (#1)
    * implemented transcription with chunking
    
    * changed duration to 5 seconds and added 1 second overlap
    
    * watson discovery backend
    
    * updated TODOs
    
    * Watson aassistant query processing
    
    * completed watson assistant backend
    
    * basic start page
    
    * basic app
    
    * upload youtube link to watson discovery
    
    * Added file upload support
    
    * moved secret key to env
    
    * working chatbot (without timestamps
    
    * refactored chatbot
    
    * chatbot always displays
    
    * Fixed chatbot positioning and responsive design
    
    * added favicon (icons8)
    
    * styled chatbot
    
    * working chatbot (need to tidy up code I've commented out)
    
    * not working
    
    * promise working
    
    * Working timestamps
    
    * refactor
    
    * fixed disappearing timestamp click event
    
    * added automatic scroll to bottom for messages

---
+++ api/src/static/chatbot.js
@@ -0,0 +1,143 @@
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector('.chatbox__button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
+        }
+        this.messages = [];
+    }
+
+    display() {
+        const {chatBox, sendButton} = this.args;
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+        chatBox.classList.add('chatbox--active')
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", ({key}) => {
+            if (key === "Enter") {
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    onSendButton(chatbox) {
+        var textField = chatbox.querySelector('input');
+        let text1 = textField.value
+        if (text1 === "") {
+            return;
+        }
+
+        let msg1 = {name: "User", message: text1}
+        this.messages.push(msg1);
+        fetch($SCRIPT_ROOT + '/watson_response', {
+            method: 'POST',
+            body: JSON.stringify({message: text1}),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(r => r.json())
+            .then(r => this.addTimeStamps(chatbox, r))
+            .then(r => {
+                    this.messages.push({name: "SmartVideo", message: r.message});
+                    this.updateChatText(chatbox, r)
+                    textField.value = ''
+                }
+            )
+            .catch((error) => {
+                console.error('Error:', error);
+                this.updateChatText(chatbox)
+                textField.value = ''
+            });
+    }
+
+    addTimeStamps(chatbox, r) {
+
+        let final_message = r.message;
+        let results = {};
+        // * if no results, append "no results found to message"
+        if (r.timestamps.length === 0) {
+            final_message += "<br><br>Unfortunately I didn't find any results."
+        }
+
+        // * if at least 1 result, write initial part of message in html with timestamp
+        // * {timestamp} may have what you are looking for.
+        if (r.timestamps.length > 0) {
+            let timestamp1_seconds = r.timestamps[0]
+            let uuid1 = generateUUID()
+            final_message += '<br><br><a id=\'' + uuid1 + '\' href=\'#\'>' + secondsToMinutes(timestamp1_seconds) + '</a> may have what you are looking for.'
+            results.timestamp1 = timestamp1_seconds
+            results.uuid1 = uuid1
+        }
+
+        // * if 2 results, write initial part of second message with timestamp
+        // * If not {timestamp2} may have what you need
+        if (r.timestamps.length > 1) {
+            let timestamp2_seconds = r.timestamps[1]
+            let uuid2 = generateUUID()
+            final_message += '<br><br>If not, <a id="' + uuid2 + '" href="#">' + secondsToMinutes(timestamp2_seconds) + '</a> may have what you need.'
+            results.timestamp2 = timestamp2_seconds
+            results.uuid2 = uuid2
+        }
+
+        results.message = final_message
+        return results
+    }
+
+    updateChatText(chatbox, results) {
+        var html = '';
+        this.messages.slice().reverse().forEach(function (item, index) {
+            if (item.name === "SmartVideo") {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+            } else {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+            }
+        });
+
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        chatmessage.insertAdjacentHTML("afterbegin", html);
+
+        //    add video jumps to timestamps
+        if (results.timestamp1 !== undefined) {
+            make_timestamps_clickable(results.uuid1, results.timestamp1)
+        }
+        if (results.timestamp2 !== undefined) {
+            make_timestamps_clickable(results.uuid2, results.timestamp2)
+        }
+        // removes the last 2 messages
+        this.messages.splice(this.messages.length - 2, 2)
+
+        // scroll to bottom of messages
+        const element = document.getElementById("chatbox_messages");
+        element.scrollTop = element.scrollHeight;
+    }
+}
+
+function generateUUID() {
+    // credit: https://github.com/IBM/audio_search_on_podcasts/blob/master/static/videojs-markers.js
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : r & 0x3 | 0x8).toString(16);
+    });
+    return uuid;
+}
+
+
+function make_timestamps_clickable(id, timestamp) {
+    const video = document.getElementById("videoDemo_html5_api"); // have to use this ID for some reason
+    document.getElementById(id).addEventListener("click", function (event) {
+        video.currentTime = timestamp;
+        video.play();
+    }, false);
+}
+
+function secondsToMinutes(s) {
+    return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s
+}
+
+const chatbox = new Chatbox();
+chatbox.display();
---
+++ api/src/watson_discovery.py
@@ -0,0 +1,77 @@
+from ibm_watson import DiscoveryV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from environment import discovery_api_key, discovery_url, \
+    discovery_environment_id, discovery_collection_id
+
+
+def setup_discovery():
+    authenticator = IAMAuthenticator(discovery_api_key)
+    discovery = DiscoveryV1(
+        version='2018-12-03',
+        authenticator=authenticator
+    )
+    discovery.set_service_url(discovery_url)
+    return discovery
+
+
+def check_if_transcript_exists(discovery, filename):
+    response = discovery.query(
+        environment_id=discovery_environment_id,
+        collection_id=discovery_collection_id
+    ).get_result()
+    for files in response["results"]:
+        if files["extracted_metadata"]["filename"] == filename:
+            return files["id"]
+    return False
+
+
+def upload_transcript(discovery, transcript_filename):
+    transcript_path = "../transcripts/" + transcript_filename
+    if check_if_transcript_exists(discovery, transcript_filename):
+        delete_transcript(discovery, check_if_transcript_exists(discovery,
+                                                                transcript_filename))
+    with open(transcript_path) as fileinfo:
+        add_doc = discovery.add_document(
+            environment_id=discovery_environment_id,
+            collection_id=discovery_collection_id,
+            file=fileinfo,
+            file_content_type="application/json"
+        ).get_result()
+        return add_doc["document_id"]
+
+
+def query_transcript(discovery, document_id, user_query):
+    chunk_duration = 5  # TODO: define these variables globally
+    result = discovery.query(
+        environment_id=discovery_environment_id,
+        collection_id=discovery_collection_id,
+        # only returns results for specific document
+        filter=f"id::{document_id}".format(document_id=document_id),
+        natural_language_query=user_query,
+        highlight=True,
+        passages=True,
+        passages_count=3  # number of passages to return
+    ).get_result()
+
+    # returns a dictionary where the key is passage text and the value is a dictionary of the score and timestamp
+    return {matching_passage["passage_text"]: {
+        "passage_score": matching_passage["passage_score"],
+        "timestamp": int(matching_passage["field"]) * chunk_duration} for
+        matching_passage in result["passages"]}
+
+
+def delete_transcript(discovery, document_id):
+    discovery.delete_document(
+        environment_id=discovery_environment_id,
+        collection_id=discovery_collection_id,
+        document_id=document_id
+    )
+
+# discovery = setup_discovery()
+
+# document_id = "b483a604-3736-4406-b88c-d3add2016b07"
+# query = "alexa"
+# print(query_transcript(discovery, document_id, query))
+
+# print(
+#     check_if_transcript_exists(discovery, "5-second_overlap_transcript.json"))
