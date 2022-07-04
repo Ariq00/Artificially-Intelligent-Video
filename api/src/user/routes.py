30f799a84e6580b3c62e213fd001da510ccca4c8
Mohammed Ariq Rahman <ariqrahman00@gmail.com>
2022-07-04 01:57:15 +0100

    Completed save video functionality

--- api/src/user/routes.py
+++ api/src/user/routes.py
@@ -11,3 +11,23 @@ def save_video():
     video_dict = request.get_json()
     Video(**video_dict, user=current_user).save()
     return video_dict
+
+
+@user_bp.route("/saved_videos", methods=["GET", "POST"])
+def saved_videos():
+    if request.args.get("videoID"):
+        video_id = request.args.get("videoID")
+        video = Video.objects(id=video_id).first()
+        return render_template("video.html",
+                               video_filepath=video.filepath,
+                               document_id=video.document_id,
+                               video_title=video.title,
+                               summary=video.summary,
+                               sentiment=video.sentiment,
+                               score=video.score,
+                               concepts=video.concepts,
+                               video_is_saved=True)
+
+    else:
+        videos = Video.objects(user=current_user)
+        return render_template("saved_videos.html", videos=videos)
